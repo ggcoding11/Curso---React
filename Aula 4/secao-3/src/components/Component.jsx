0dc1c17d7f21de0f3c1dcde606076c0a311cb734
Gustavo <gustavogomes1108@gmail.com>
2025-10-24 13:56:11 -0300

    .

--- Aula 4/secao-3/src/components/Component.jsx
+++ Aula 4/secao-3/src/components/Component.jsx
@@ -1,8 +1,28 @@
-import Foto from "../assets/foto1.jpg"
+import Foto from "../assets/foto1.jpg";
+import { useState } from "react";
+function Component() {
+  const [nome, mudarNome] = useState("Gustavo");
 
-function Component(){
-  return(
-    <img src={Foto} alt="Paisagem" />
+  let numbers = [64, 49, 4, 16];
+
+  let novoArray = numbers.map(Math.sqrt);
+
+  console.log(novoArray);
+
+  return (
+    <div>
+      <img src={Foto} alt="Paisagem" />
+
+      <p>Valor: {nome}</p>
+
+      <button
+        onClick={() => {
+          mudarNome("Mudou o nome");
+        }}
+      >
+        Aumentar
+      </button>
+    </div>
   );
 }
 
