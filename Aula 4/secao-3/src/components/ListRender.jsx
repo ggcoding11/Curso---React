16942413a435a753a345bad41e26ae1e4ab13a49
Gustavo <gustavogomes1108@gmail.com>
2025-10-29 14:06:45 -0300

    >

--- Aula 4/secao-3/src/components/ListRender.jsx
+++ Aula 4/secao-3/src/components/ListRender.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
 const ListRender = () => {
-  const [list] = useState([1, 2, 3, 4, 5, 6]);
+  // const [list] = useState([1, 2, 3, 4, 5, 6]);
+  const list = [1, 2, 3, 4, 5, 6];
 
   return (
     <div>
+      {/* <ul>{list.map((item) => {
+        return <li> {item} </li>
+      })}</ul> */}
+
       <ul>
         {list.map((item) => (
-          <li>{item}</li>
+          <li> {item} </li>
         ))}
       </ul>
     </div>
