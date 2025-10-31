58a599fe7775af72eb9506cd5752cc8ce23b1f25
JeeJ <155782096+ggcoding11@users.noreply.github.com>
2025-10-31 15:29:05 -0300

    ;

--- Aula 4/secao-3/src/components/ListRender.jsx
+++ Aula 4/secao-3/src/components/ListRender.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 
 const ListRender = () => {
-  const [list, mudarList] = useState([1, 2, 3, 4, 5, 6]);
+  // const [list, mudarList] = useState([1, 2, 3, 4, 5, 6]);
 
+  const list = [
+    {
+      id: 1,
+      nome: "Gustavo",
+    },
+
+    {
+      id: 2,
+      nome: "Ana",
+    },
+
+    {
+      id: 3,
+      nome: "Júlia",
+    },
+
+    {
+      id: 4,
+      nome: "Carlos",
+    },
+  ];
   return (
     <div>
       {/* <ul>{list.map((item) => {
@@ -10,8 +31,8 @@ const ListRender = () => {
       })}</ul> */}
 
       <ul>
-        {list.map((item, i) => (
-          <li> {item} </li>
+        {list.map((item) => (
+          <li key={item.id}> {item.nome} </li>
         ))}
       </ul>
     </div>
