cc5d125aff3b79359f520e1878d789a949b447ba
Gustavo <gustavogomes1108@gmail.com>
2025-12-09 12:54:24 -0300

    >

--- React + CSS/react+css]/src/App.jsx
+++ React + CSS/react+css]/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div className="container">
       <h1>React + CSS</h1>
+      <p style={{color: "red", fontSize:"20px"}}>Esse é o parágrafo</p>
     </div>
   );
 };
