962d3fb1c0548a0d6fd7bbba18bd7ba5cf17c6b2
Gustavo <gustavogomes1108@gmail.com>
2025-12-09 13:29:28 -0300

    >

--- React + CSS/react+css]/src/App.jsx
+++ React + CSS/react+css]/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.css";
 
 const App = () => {
+  let redTitle = false;
+
   return (
     <div className="container">
       <h1>React + CSS</h1>
-      <p style={{color: "red", fontSize:"20px"}}>Esse é o parágrafo</p>
+      <p style={{ color: "red", fontSize: "20px" }}>Esse é o parágrafo</p>
+
+      <h2 className={redTitle ? "red-title" : "blue-title"}>CSS Dinâmico</h2>
     </div>
   );
 };
---
+++ React + CSS/react+css]/src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <p>Pedrinho</p>
+    </div>
+  );
+};
+
+export default Title;
