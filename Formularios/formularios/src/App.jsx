6837020123cd7ac7be174d1052339cde79e50f28
Gustavo <gustavogomes1108@gmail.com>
2025-12-12 13:41:22 -0300

    >

--- Formularios/formularios/src/App.jsx
+++ Formularios/formularios/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MyForm from "./components/MyForm"
+import "./App.css"
+
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <MyForm></MyForm>
     </div>
   );
--- Formularios/formularios/src/components/MyForm.jsx
+++ Formularios/formularios/src/components/MyForm.jsx
@@ -1,25 +1,40 @@
-import React from 'react'
-import "../components/MyForm.css"
+import React from "react";
+import { useState } from "react";
+import "../components/MyForm.css";
 
 const MyForm = () => {
+  const [nome, setNome] = useState();
+  const [idade, setIdade] = useState();
+
+  const handleNome = (e) => {
+    setNome(e.target.value);
+  };
+
+  console.log(nome)
+
   return (
-    <div className="container">
+    <div className="container-form">
       <h1>Formulário de teste</h1>
 
       <form action="" className="formulario">
         <div>
           <label htmlFor="nome">Nome: </label>
-          <input type="text" name="" id="nome" />
+          <input
+            type="text"
+            name=""
+            id="nome"
+            placeholder="Digite o seu nome"
+            onChange={handleNome}
+          />
         </div>
 
         <div>
           <label htmlFor="idade">Idade: </label>
-          <input type="number" name="" id="idade" min={0} max={100}/>
+          <input type="number" name="" id="idade" min={0} max={100} />
         </div>
-
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
