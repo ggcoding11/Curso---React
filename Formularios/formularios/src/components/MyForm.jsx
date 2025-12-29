3d2f0ffb0cceba5e7de6d72a842af39872d7e406
Gustavo <gustavogomes1108@gmail.com>
2025-12-29 17:20:22 -0300

    >

--- Formularios/formularios/src/components/MyForm.jsx
+++ Formularios/formularios/src/components/MyForm.jsx
@@ -3,14 +3,11 @@ import { useState } from "react";
 import "../components/MyForm.css";
 
 const MyForm = () => {
-  const [nome, setNome] = useState();
-  const [idade, setIdade] = useState();
+  const [nome, setNome] = useState("");
 
-  const handleNome = (e) => {
+  const mudarNome = (e) => {
     setNome(e.target.value);
   };
-
-  console.log(nome)
 
   return (
     <div className="container-form">
@@ -24,13 +21,32 @@ const MyForm = () => {
             name=""
             id="nome"
             placeholder="Digite o seu nome"
-            onChange={handleNome}
+            onChange={mudarNome}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="nome2">Nome: </label>
+          <input
+            type="text"
+            name=""
+            id="nome2"
+            placeholder="Digite o seu nome"
+            onChange={mudarNome}
+            value={nome}
           />
         </div>
 
         <div>
           <label htmlFor="idade">Idade: </label>
-          <input type="number" name="" id="idade" min={0} max={100} />
+          <input
+            type="number"
+            name=""
+            id="idade"
+            min={0}
+            max={100}
+            placeholder="Digite sua idade"
+          />
         </div>
       </form>
     </div>
--- React + CSS/react+css/src/App.jsx
+++ React + CSS/react+css/src/App.jsx
@@ -1,12 +1,56 @@
 import React from "react";
 import Title from "./components/Title";
+import Foto from "../src/assets/react.svg";
+import { useState } from "react";
 
 const App = () => {
+  let nome = "Gustavo";
+
+  function renderNome() {
+    return (
+      <div>
+        <p>Olá, meu nome é JeeJ</p>
+      </div>
+    );
+  }
+
+  let array = [2, 4, 6, 8];
+
+  function duplicar(n) {
+    return n * 2;
+  }
+
+  let novoArray = array.map(duplicar);
+
+  console.log(novoArray);
+
+  const [cont, setCont] = useState(0);
+
   return (
     <div>
       <Title></Title>
 
-      <p>Olá</p>
+      <p>Olá, {nome}</p>
+
+      {renderNome()}
+
+      <ul>
+        {novoArray.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+
+      <img src="/vite.svg" alt="" />
+
+      <button
+        onClick={() => {
+          setCont(cont + 1);
+        }}
+      >
+        Clique em mim
+      </button>
+
+      <p>Valor atual = {cont}</p>
     </div>
   );
 };
