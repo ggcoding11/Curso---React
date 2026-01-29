b1143ec08a91c0af6fc1798a32f656f753cf58c0
Gustavo <gustavogomes1108@gmail.com>
2026-01-29 17:02:11 -0300

    .

--- Leitura de API/leitura-api/src/App.jsx
+++ Leitura de API/leitura-api/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+
+const url = `http://localhost:3000/products`;
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default App
+  useEffect(() => {
+    async function readAPI() {
+      const promise = await fetch(url);
+
+      const json = await promise.json();
+
+      setProducts(json);
+    }
+
+    readAPI();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div>
+      <h1>Lista de produtos</h1>
+    </div>
+  );
+};
+
+export default App;
